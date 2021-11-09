207de1a55b65be9f1d73138242c2527fca31100d
1140480798 <73691756+1140480798@users.noreply.github.com>
2021-11-09 10:56:41 +0800

    11/9

--- src/components/page/student/comprehensive/ComprehensiveReviewing.vue
+++ src/components/page/student/comprehensive/ComprehensiveReviewing.vue
@@ -1,68 +1,19 @@
 <template>
   <!--  待重构  -->
   <div>
+<!--    title     -->
     <h1 align="center">{{currentAcademicYear}}年综合测评</h1>
-    <div>
-      <el-row class="basicExih">
-        <el-col :span="4"><b>加权成绩</b></el-col>
-        <el-col :span="20">{{basicInfo.grade}}</el-col>
-      </el-row>
-      <el-row class="basicExih">
-        <el-col :span="4"><b>本学年所修学分</b></el-col>
-        <el-col :span="20">{{basicInfo.currentCredit}}</el-col>
-      </el-row>
-      <el-row class="basicExih">
-        <el-col :span="4"><b>挂科数</b></el-col>
-        <el-col :span="20">{{basicInfo.failedNumber}}</el-col>
-      </el-row>
-      <el-row class="basicExih">
-        <el-col :span="4"><b>德育分</b></el-col>
-        <el-col :span="20">{{basicInfo.moralScore}}</el-col>
-      </el-row>
-      <el-row class="basicExih">
-        <el-col :span="4"><b>综合素质加分合计</b></el-col>
-        <el-col :span="20">{{basicInfo.bonus}}</el-col>
-      </el-row>
-      <el-row class="basicExih">
-        <el-col :span="24"><b>综合加分细则</b></el-col>
-      </el-row>
-      <el-row>
-          <el-table
-          border
-          :span-method="tableSpanMethod"
-          :data="data">
-            <el-table-column
-              label="加分类别"
-              prop="itemType">
-            </el-table-column>
-            <!--<el-table-column-->
-              <!--label="类别总分"-->
-              <!--prop="typeTotal">-->
-            <!--</el-table-column>-->
-            <el-table-column
-              label="加分项名"
-              prop="itemName">
-            </el-table-column>
-            <el-table-column
-              label="分值"
-              prop="score">
-            </el-table-column>
-            <el-table-column label="状态">
-              <template slot-scope="scope">
-                <el-tag>{{scope.row.status}}</el-tag>
-              </template>
-            </el-table-column>
-          </el-table>
-      </el-row>
-      <!--<el-row class="basicExih">-->
-        <!--<el-col :span="4"><b>加分成绩</b></el-col>-->
-        <!--<el-col :span="20">{{basicInfo.bonusScore}}</el-col>-->
-      <!--</el-row>-->
-      <el-row class="basicExih">
-        <el-col :span="4"><b>最终成绩</b></el-col>
-        <el-col :span="20">{{basicInfo.finalScore}}</el-col>
-      </el-row>
-    </div>
+<!--    总表格    -->
+    <total-reviewing
+      :grade = 'basicInfo.grade'
+      :current-credit = 'basicInfo.currentCredit'
+      :failed-number = 'basicInfo.failedNumber'
+      :moral-score = 'basicInfo.moralScore'
+      :bonus = 'basicInfo.bonus'
+      :final-score = 'basicInfo.finalScore'
+      :data = 'data'
+    />
+<!--    返回按钮    -->
     <div style="padding-left: 50%;">
       <el-button type="primary" @click="back">返回</el-button>
     </div>
@@ -70,8 +21,10 @@
 </template>
 
 <script>
+import TotalReviewing from "./elements/TotalReviewing";
 export default {
   name:"comprehensivereviewing",
+  components: {TotalReviewing},
   data(){
     return{
       //reviewed:'',   //是否审核过了
---
+++ src/components/page/student/comprehensive/elements/TotalReviewing.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <el-row class="basicExih">
+      <el-col :span="4"><b>加权成绩</b></el-col>
+      <el-col :span="20">{{grade}}</el-col>
+    </el-row>
+    <el-row class="basicExih">
+      <el-col :span="4"><b>本学年所修学分</b></el-col>
+      <el-col :span="20">{{currentCredit}}</el-col>
+    </el-row>
+    <el-row class="basicExih">
+      <el-col :span="4"><b>挂科数</b></el-col>
+      <el-col :span="20">{{failedNumber}}</el-col>
+    </el-row>
+    <el-row class="basicExih">
+      <el-col :span="4"><b>德育分</b></el-col>
+      <el-col :span="20">{{moralScore}}</el-col>
+    </el-row>
+    <el-row class="basicExih">
+      <el-col :span="4"><b>综合素质加分合计</b></el-col>
+      <el-col :span="20">{{bonus}}</el-col>
+    </el-row>
+    <el-row class="basicExih">
+      <el-col :span="24"><b>综合加分细则</b></el-col>
+    </el-row>
+    <el-row>
+      <el-table
+        border
+        :span-method="tableSpanMethod"
+        :data="data">
+        <el-table-column
+          label="加分类别"
+          prop="itemType">
+        </el-table-column>
+        <!--<el-table-column-->
+        <!--label="类别总分"-->
+        <!--prop="typeTotal">-->
+        <!--</el-table-column>-->
+        <el-table-column
+          label="加分项名"
+          prop="itemName">
+        </el-table-column>
+        <el-table-column
+          label="分值"
+          prop="score">
+        </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag>{{scope.row.status}}</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
+    <!--<el-row class="basicExih">-->
+    <!--<el-col :span="4"><b>加分成绩</b></el-col>-->
+    <!--<el-col :span="20">{{bonusScore}}</el-col>-->
+    <!--</el-row>-->
+    <el-row class="basicExih">
+      <el-col :span="4"><b>最终成绩</b></el-col>
+      <el-col :span="20">{{finalScore}}</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'grade',
+    'currentCredit',
+    'failedNumber',
+    'moralScore',
+    'bonus',
+    'finalScore',
+    'data'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
